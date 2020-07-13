74c4e560150096a6cdbf208ac31ffa0ceeb30835
ostrovskyi <andrii.ostrovskyi@maxpay.com>
2020-07-13 17:18:00 +0300

    Add order_item, order_submit event type

--- tests/Covery/BuildOrderSubmitEnvelopeTest.php
+++ tests/Covery/BuildOrderSubmitEnvelopeTest.php
@@ -1,6 +1,6 @@
 <?php
 
-class BuildOrderItemEnvelopeTest extends \PHPUnit_Framework_TestCase
+class BuildOrderSubmitEnvelopeTest extends \PHPUnit_Framework_TestCase
 {
     public function testBuild()
     {
